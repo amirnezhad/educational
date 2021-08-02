f6efc9b1c77d3b21174290854e1de58827b087fd
marjan <m.amiri.js@gmail.com>
2021-08-02 14:02:34 +0430

    scroll-top

--- content/js/parsian.js
+++ content/js/parsian.js
@@ -104,66 +104,22 @@ $(document).ready(function() {
         ]
 
     });
+    $(window).scroll(function() {
+
+        var scroll = $(window).scrollTop();
+        if (scroll > 2000) {
+
+
+            $(".top").fadeIn(2000);
+        } else { $(".top").fadeOut(2000); }
+
+
+    });
+
+    $(".top").click(function() {
+
+        $("html , body").animate({ scrollTop: 0 }, 3000);
+    });
 
 
 });
-
-
-
-
-// $(document).ready(function() {
-//     $('.one-time').slick({
-//         dots: true,
-//         infinite: true,
-//         speed: 300,
-//         slidesToShow: 1,
-//         adaptiveHeight: true
-//     });
-
-// $(".hero-slider").slick({
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 2100,
-//     speed: 1000,
-//     dots: true,
-//     dotsClass: 'slide-dots',
-//     arrows: false,
-//     centerMode: true,
-//     centerPadding: '0px',
-// });
-
-// var swiper1 = new Swiper(".swiper-top", {
-
-//     pagination: {
-//         el: ".swiper-pagination",
-//         dynamicBullets: true,
-//         clickable: true,
-//     },
-// });
-
-
-//     $('.autoplay').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         autoplay: true,
-//         autoplaySpeed: 2000,
-//     });
-
-
-//     $('.slider-nav').slick({
-//         slidesToShow: 3,
-//         slidesToScroll: 1,
-//         asNavFor: '.slider-for',
-//         dots: true,
-//         focusOnSelect: true
-//     });
-
-//     $('a[data-slide]').click(function(e) {
-//         e.preventDefault();
-//         var slideno = $(this).data('slide');
-//         $('.slider-nav').slick('slickGoTo', slideno - 1);
-//     });
-
-
-// });
