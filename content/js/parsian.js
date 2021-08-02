a64eb7b71b4989878bcebc1f102b5cf206fce91d
marjan <m.amiri.js@gmail.com>
2021-08-02 09:43:03 +0430

    comiit

--- content/js/parsian.js
+++ content/js/parsian.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+    const typeWriter = selector => {
+        const el = document.querySelector(selector)
+        const text = el.innerHTML
+
+        ;
+        (function _type(i = 0) {
+            if (i === text.length) return
+
+            el.innerHTML =
+                text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
+            setTimeout(() => _type(i + 1), 100)
+        })()
+    }
+
+    typeWriter(".js-type-writer")
+
+
+
+
+
+
+
 
     $(".heroo-slider").slick({
         slidesToShow: 1,
